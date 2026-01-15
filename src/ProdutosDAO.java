4c5e40ed7e13d8feb3fbdd526dc37822c445fddb
Felipe <89211047+rochafelilpe@users.noreply.github.com>
2026-01-15 19:07:51 -0300

    Funcionalidade de cadastro adicionada ao projeto, bem como as mensagems de confirmação

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -10,22 +10,42 @@
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.sql.DriverManager; 
+import java.sql.SQLException; 
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
 
 public class ProdutosDAO {
-    
-    Connection conn;
-    PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
+    
     public void cadastrarProduto (ProdutosDTO produto){
+        conectaDAO conectaDAO = new conectaDAO();
+        Connection conexao = conectaDAO.connectDB();
         
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)";
+        try(conexao; PreparedStatement prep = conexao.prepareStatement(sql)){
+            
+            //preenche os parâmetros do sql 
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            
+            //rodando na base de dados
+            int linhasAfetadas = prep.executeUpdate();
+            
+            if(linhasAfetadas > 0){
+                JOptionPane.showMessageDialog(null, "Produto cadastrado!");
+            }
+            
+            
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "Erro ao conectar no banco ou cadastrar o produto!" + sqle.getMessage());
+        }
         
-        //conn = new conectaDAO().connectDB();
         
         
     }
